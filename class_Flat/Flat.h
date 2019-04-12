7e9bae85953c39819c0926d874db37fcf4cbf9d8
Sasha Goryachev <45205367+Donyoku2k13@users.noreply.github.com>
2019-04-12 19:02:43 +0600

    Update Flat.h

--- class_Flat/Flat.h
+++ class_Flat/Flat.h
@@ -1,14 +1,52 @@
+#pragma once
 #include "Room.h"
-class Flat_
-{
-private:
-	int area;
-	room_ *rooms;
+#include <iostream>
+using namespace std;
+
+class flat {
 	int price;
+	room *rooms;
+	int cntRooms;
 public:
-	Flat_();
-	~Flat_();
+	flat(int price) {
+		this->price = price;
+		this->rooms = nullptr;
+		this->cntRooms = 0;
+	}
+	void addRoom(const room &r) {
+		if (this->rooms == nullptr) {
 
+			this->rooms = new room[1];
 
+			rooms[0].rewrite(r);
+			cntRooms = 1;
+			return;
+		}
+		room *tmp = new room[cntRooms + 1];
+		for (size_t i = 0; i < cntRooms; i++)
+			tmp[i].rewrite(rooms[i]);
+
+		tmp[cntRooms].rewrite(r);
+		delete[]rooms;
+		rooms = tmp;
+		cntRooms++;
+	}
+	int getFlatArea() const {
+		int area = 0;
+		for (size_t i = 0; i < cntRooms; i++)
+		{
+			area += rooms[i].getArea();
+		}
+		return area;
+	}
+	void info() {
+		cout << "Flat amount = " << this->price
+			<< "\ncount of rooms = " << cntRooms
+			<< "flat area = " << this->getFlatArea()
+			<< "rooms:\n";
+		for (size_t i = 0; i < cntRooms; i++)
+		{
+			rooms[i].info();
+		}
+	}
 };
-
